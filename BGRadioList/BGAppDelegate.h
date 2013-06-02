8cf883b32f184e085ff1f110ae4d114360005911
Bishal Ghimire <bishal.ghimire@gmail.com>
2013-06-02 17:29:00 +0545

    Initial Commit

---
+++ BGRadioList/BGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BGAppDelegate.h
+//  BGRadioList
+//
+//  Created by Bishal Ghimire on 6/2/13.
+//  Copyright (c) 2013 Bishal Ghimire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BGRadioList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BGRadioList
+//
+//  Created by Bishal Ghimire on 6/2/13.
+//  Copyright (c) 2013 Bishal Ghimire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BGAppDelegate class]));
+    }
+}
